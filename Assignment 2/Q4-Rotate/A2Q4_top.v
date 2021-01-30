2194860135d98077cc759fab4442a5c5c46ebe4f
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-01-31 02:02:44 +0530

    Add A2Q4 solution

---
+++ Assignment 2/Q4-Rotate/A2Q4_top.v
@@ -0,0 +1,24 @@
+`include "./A2Q4_rotate.v"
+
+module top;
+
+reg clk;
+wire [3:0] out;
+
+rotate ROTATE(clk, out);
+
+always @(out) begin
+    $display("Time: %d | Output: %b", $time, out);
+end
+
+initial begin
+    forever begin
+        clk = 0;
+        #5
+        clk = 1;
+        #5
+        clk = 0;
+    end
+end
+
+endmodule
