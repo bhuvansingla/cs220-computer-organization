0f2484c71009a77b440001afd1e4a64b1f12ba2a
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-02-03 20:50:10 +0530

    Update A2Q4

--- Assignment 2/Q4-Rotate/A2Q4_rotate.v
+++ Assignment 2/Q4-Rotate/A2Q4_rotate.v
@@ -3,21 +3,19 @@ module rotate(clk, out);
 input clk;
 output reg [3:0] out = 4'b1000;
 
-reg [21:0] cycles = 0;
+reg [15:0] cycles = 0;
 
 reg temp;
 
 always @(posedge clk) begin
-    cycles <= cycles + 1;
-    if (cycles % 25000 == 0) begin
+    cycles = cycles + 1;
+    if (cycles == 25000) begin
       temp = out[3];
-      out[3] = out[2]; 
-      out[2] = out[1]; 
-      out[1] = out[0]; 
-      out[0] = temp; 
-    end
-    if (cycles == 310000) begin
-      $finish;
+      out[3] = out[2];
+      out[2] = out[1];
+      out[1] = out[0];
+      out[0] = temp;
+      cycles = 0;
     end
 end
 
