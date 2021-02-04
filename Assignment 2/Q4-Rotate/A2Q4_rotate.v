112d5e74bd54109da1407d3db90b3914df677c9c
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-02-04 15:13:42 +0530

    Update A2Q4

--- Assignment 2/Q4-Rotate/A2Q4_rotate.v
+++ Assignment 2/Q4-Rotate/A2Q4_rotate.v
@@ -5,17 +5,14 @@ output reg [3:0] out = 4'b1000;
 
 reg [15:0] cycles = 0;
 
-reg temp;
-
 always @(posedge clk) begin
-    cycles = cycles + 1;
+    cycles <= cycles + 1;
     if (cycles == 25000) begin
-      temp = out[3];
-      out[3] = out[2];
-      out[2] = out[1];
-      out[1] = out[0];
-      out[0] = temp;
-      cycles = 0;
+      out[3] <= out[2];
+      out[2] <= out[1];
+      out[1] <= out[0];
+      out[0] <= out[3];;
+      cycles <= 1;
     end
 end
 
